8e3966d2d7dbf9ddba822fc6109c39e121ce4d51
DESKTOP-87P3H06\Kaushik <kaushik@qseksolutions.com>
2018-06-08 19:12:45 +0530

    design login otp verify(kaushik)

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { DesaifamilyComponent } from './desaifamily/desaifamily';
+@NgModule({
+	declarations: [DesaifamilyComponent],
+	imports: [],
+	exports: [DesaifamilyComponent]
+})
+export class ComponentsModule {}
