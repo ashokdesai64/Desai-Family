cc2b2a9c616954e9215f0f5f30ac96d1f6dbf86a
praful <praful@qseksolutions.com>
2018-06-13 11:17:18 +0530

    add image upload with view on result page

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ResultPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Camera } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -14,12 +8,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'result.html',
 })
 export class ResultPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  base64Image: any;
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public camera: Camera,
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResultPage');
   }
 
+  accessGallery() {
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then((imageData) => {
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
